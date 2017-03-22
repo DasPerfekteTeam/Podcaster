a3ce9026bbcc1df81bce0a46ad049b2172f89799
Christian Mueller <christian@flownative.com>
2017-03-22 01:44:09 +0100

    BUGFIX: More robust loading order/execution of JavaScript

--- Resources/Public/JavaScript/init.js
+++ Resources/Public/JavaScript/init.js
@@ -1,5 +1,7 @@
 (function ($) {
-    $('.podcaster-podlove-web-player').each(function () {
-        $(this).podlovewebplayer($(this).data('podlove-web-player'));
+    $(function() {
+        $('.podcaster-podlove-web-player').each(function () {
+            $(this).podlovewebplayer($(this).data('podlove-web-player'));
+        });
     });
 })(jQuery);
